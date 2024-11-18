4a8ee8fd4fcfd864a4786b241d6e4ba5f563257c
hungnp <hung.nguyen@digime.asia>
2024-11-18 19:29:41 +0700

    Update dev env setup.

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import './styles.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <Header />
+      <div className="content-container">
+        <Sidebar />
+        <MainContent />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
